0c21d404a58e2cd0a1ec4f794e0b112ea5f16cba
Pedro Siqueira <pedrohenriquedoamaralsiqueira@gmail.com>
2023-10-28 21:03:37 -0300

    Better courses section on the main page

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,7 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     screens: {
+      "smallPhone": "300px",
       "mobile": "481px",
       "tablet": "768px",
       "Ltablet": "992px",
